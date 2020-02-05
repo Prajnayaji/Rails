3cbe3fdfaaab215c1b2eae960c86253fe1a8c8d5
Prajnayaji <prajnayaji@gmail.com>
2020-02-05 10:47:34 +0530

    addded

---
+++ chatapp/app/controllers/messages_controller.rb
@@ -0,0 +1,12 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.save
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
---
+++ chatapp/test/jobs/message_boradcast_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MessageBoradcastJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
